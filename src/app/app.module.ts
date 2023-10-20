33e4602a4c0a33876ffab0767b9d075483156808
Nikola Georgiev <kvabakoma@users.noreply.github.com>
2023-10-20 18:39:26 +0300

    WIP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,16 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
+import { HomeComponent } from './home/home.component'
+import { LoginComponent } from './login/login.component'
+import { RegisterComponent } from './register/register.component'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { TestComponentComponent } from './test-component/test-component.component'
 
-import { authInterceptorProviders } from './interceptors/auth.interceptor';
+import { authInterceptorProviders } from './interceptors/auth.interceptor'
 
 @NgModule({
-    declarations: [AppComponent, TestComponentComponent],
+    declarations: [AppComponent, TestComponentComponent, HomeComponent, LoginComponent, RegisterComponent],
     imports: [
         BrowserModule,
         AppRoutingModule,
