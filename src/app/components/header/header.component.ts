1d2836fb2b83dee0899ff5cd528854acd1e2a7d6
Nikola Georgiev <kvabakoma@users.noreply.github.com>
2023-10-21 14:37:41 +0300

    Header component (#3)
    * Created Header Component
    
    * Header is functional

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, Input } from '@angular/core'
+import { UserService } from '../../services/user.service';
+
+@Component({
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.css'],
+    standalone: false,
+    // imports: [MatToolbarModule, MatButtonModule, MatIconModule, CommonModule],
+})
+export class HeaderComponent implements OnInit {
+
+  @Input() pagetitle!: string;
+  @Input() subtitle!: string;
+  @Input() showPhoto!: boolean;
+
+  constructor(private userService: UserService) {
+
+  }
+
+  ngOnInit(): void {
+    console.log("showPhoto: ", this.showPhoto)
+  }
+}
