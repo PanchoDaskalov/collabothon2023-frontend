e315b55c2e535a44a8079cf0b6abc12ee128b287
Nikola Georgiev <kvabakoma@gmail.com>
2023-10-22 02:08:39 +0300

    Wizard Component Styled

--- src/app/components/wizard/wizard.component.ts
+++ src/app/components/wizard/wizard.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Directionality } from '@angular/cdk/bidi';
+import { Component, ViewChild, OnInit } from '@angular/core'
+import { MatStepper } from '@angular/material/stepper'
 
 @Component({
-  selector: 'app-wizard',
-  templateUrl: './wizard.component.html',
-  styleUrls: ['./wizard.component.css']
+    selector: 'app-wizard',
+    templateUrl: './wizard.component.html',
+    styleUrls: ['./wizard.component.css'],
 })
-export class WizardComponent {
+export class WizardComponent implements OnInit {
+    // @ViewChild('stepper') stepper: MatStepper
 
+    constructor() {      
+      
+    }
+
+    ngOnInit(): void {
+      // this.stepper = new MatStepper;
+    }
+
+    /* onStartClicked() {
+        // complete the current step
+        this.stepper.selected.completed = true
+        // move to next step
+        this.stepper.next()
+    } */
 }
+
+// https://stackoverflow.com/questions/46933953/angular-material-stepper-disable-header-navigation
